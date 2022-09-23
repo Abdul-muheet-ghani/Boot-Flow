57ead7bc832bd9f2007153395cba662e3918d1d8
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-09-23 07:36:10 +0000

    assembly update

--- linker/source/c_test_assembly.s
+++ linker/source/c_test_assembly.s
@@ -5,40 +5,25 @@
 	.attribute stack_align, 16
 	.text
 	.align	1
-	.globl	mult
-	.type	mult, @function
-mult:
+	.globl	main
+	.type	main, @function
+main:
 	addi	sp,sp,-32
 	sd	s0,24(sp)
 	addi	s0,sp,32
-	li	a5,1000
+	li	a5,100
 	sw	a5,-20(s0)
 	li	a5,3
 	sw	a5,-24(s0)
 	lw	a5,-20(s0)
 	mv	a4,a5
 	lw	a5,-24(s0)
-	mulw	a5,a4,a5
-	sext.w	a5,a5
+	addw	a5,a4,a5
+	sw	a5,-28(s0)
+	lw	a5,-28(s0)
 	mv	a0,a5
 	ld	s0,24(sp)
 	addi	sp,sp,32
-	jr	ra
-	.size	mult, .-mult
-	.align	1
-	.globl	main
-	.type	main, @function
-main:
-	addi	sp,sp,-16
-	sd	ra,8(sp)
-	sd	s0,0(sp)
-	addi	s0,sp,16
-	call	mult
-	li	a5,0
-	mv	a0,a5
-	ld	ra,8(sp)
-	ld	s0,0(sp)
-	addi	sp,sp,16
 	jr	ra
 	.size	main, .-main
 	.ident	"GCC: (g1ea978e3066) 12.1.0"
