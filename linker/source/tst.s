459ebf5790f1b69883eef1508be01ec323b471dd
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-09-25 18:41:31 +0000

    updated files

---
+++ linker/source/tst.s
@@ -0,0 +1,29 @@
+	.file	"tst.c"
+	.option nopic
+	.attribute arch, "rv64i2p0_f2p0_d2p0"
+	.attribute unaligned_access, 0
+	.attribute stack_align, 16
+	.text
+	.align	2
+	.globl	main
+	.type	main, @function
+main:
+	addi	sp,sp,-32
+	sd	s0,24(sp)
+	addi	s0,sp,32
+	li	a5,10
+	sw	a5,-20(s0)
+	li	a5,2
+	sw	a5,-24(s0)
+	lw	a5,-20(s0)
+	mv	a4,a5
+	lw	a5,-24(s0)
+	addw	a5,a4,a5
+	sw	a5,-28(s0)
+	lw	a5,-28(s0)
+	mv	a0,a5
+	ld	s0,24(sp)
+	addi	sp,sp,32
+	jr	ra
+	.size	main, .-main
+	.ident	"GCC: (g1ea978e3066) 12.1.0"
