45677335e176a7e36544c78779236fdb8daee8d9
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-09-25 18:40:44 +0000

    Pointer usage

--- linker/source/tst.c
+++ linker/source/tst.c
@@ -1,5 +1,8 @@
 int main() {
-        int a=100,b=3;
-        int c;
-        return c = a+b;
+    int a=10;
+    int b=2;
+    int c=0x8C000000;
+   int* ptr_c = (int*)(c);
+    *ptr_c = a+b;
+    return 0;
 }
