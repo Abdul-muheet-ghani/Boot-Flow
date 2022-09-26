1b690db9ed5d67455cacde4a87f02dfb4db23e41
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-09-26 19:46:32 +0000

    updating

--- linker/SiFive/exm.c
+++ linker/SiFive/exm.c
@@ -1,12 +1,13 @@
 #include <stdint.h>
+#include <stdio.h>
+
 #define a (*(int *)0x8C000000)
 #define b (*(int *)0x8C000004)
 #define c (*(int *)0x8C000008)
 #define e (*(int *)0x88000000)
 
-
 int main() {
-    char d[80] = "helo";
+    char d[50] = {'h','e'};
     e = d;
     a=10;
     b=2;
