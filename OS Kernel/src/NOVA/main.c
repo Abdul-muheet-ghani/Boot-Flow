75494e63b391e02e6876afcc63c828d4f8b883e1
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-11-03 17:34:17 +0000

    update main

--- OS Kernel/src/NOVA/main.c
+++ OS Kernel/src/NOVA/main.c
@@ -1,18 +1,14 @@
 #include <stdint.h>
 #include <stdio.h>
-#include "print.h"
+#include <stdc++.h>
+using namespace std;
 
 #define a (*(int *)0x8C000000)
 #define b (*(int *)0x8C000004)
 #define c (*(int *)0x8C000008)
 #define e (*(int *)0x88000000)
 
-int kernel_main() {
-    char d[50] = {'h','e'};
-    e = d;
-    a=10;
-    b=2;
-    c = b+a;
-    print_str("abc");
+int main() {
+    cout<<"a";
     return 0;
 }
