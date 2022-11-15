b943e01b03981655b77585ef40b0932848ea16c3
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-11-15 17:17:01 +0000

    fork sample file

---
+++ OS System Calls/Fork.c
@@ -0,0 +1,22 @@
+// C-language-Practice Code
+// github link : (https://github.com/Abdul-muheet-ghani/C-Language-Practice.git)
+// E-MAIL      : (aghani@students.uit.edu)
+// Description : Hands on Practice of System C
+// Author      : Abdul Muheet Ghani 
+// Date        : 28/OCT/2022
+
+
+
+
+
+
+#include <stdio.h>
+int main(){
+    // variables 
+    fork();
+    fork();
+    fork();
+    fork();
+    printf("\nHello");
+    return 0;
+}
